29f568e08c842b96213b5ee58d6120ce418b0724
Meng Zhou <mengzhou@usc.edu>
2013-07-12 00:54:44 +0000

    Added SAM.cpp and SAM.hpp to smithlab_cpp

---
+++ SAM.cpp
@@ -0,0 +1,247 @@
+/*
+ *    Part of SMITHLAB_CPP software
+ *
+ *    Copyright (C) 2013 University of Southern California and
+ *                       Andrew D. Smith
+ *
+ *    Authors: Meng Zhou, Qiang Song, Andrew Smith
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "SAM.hpp"
+#include "smithlab_utils.hpp"
+#include "MappedRead.hpp"
+
+#include <fstream>
+
+using std::string;
+using std::vector;
+
+SAM::SAM(string mapper) : mapper(mapper) {}
+
+SAM::SAM(string mapper, string line) : mapper(mapper) {
+  std::istringstream iss(line);
+  if (mapper.compare("bsmap") == 0) {
+    if (!(iss >> name >> flag >> chrom >> start >> mapq_score >> CIGAR
+        >> mate_name >> mate_start >> seg_len >> seq >> qual
+        >> mismatch_str >> strand_str))
+      throw SMITHLABException("malformed line in bsmap SAM format:\n" + line);
+
+    get_mismatch_bsmap();
+  }
+  else if (mapper.compare("bismark") == 0) {
+    if (!(iss >> name >> flag >> chrom >> start >> mapq_score >> CIGAR
+        >> mate_name >> mate_start >> seg_len >> seq >> qual
+        >> edit_distance_str >> mismatch_str >> meth_call_str
+        >> read_conv_str >> genome_conv_str))
+      throw SMITHLABException("malformed line in bsmap SAM format:\n" + line);
+
+    get_mismatch_bismark();
+  }
+  else {
+    throw SMITHLABException("Unsupported mapper for SAM format: " + mapper);
+  }
+}
+
+MappedRead
+SAM::GetMappedRead() const {
+  MappedRead mr;
+  //SAM is 1-based
+  GenomicRegion r(chrom,start - 1,start - 1 + seq.size());
+  
+  string new_seq, new_qual;
+  apply_CIGAR(new_seq, new_qual);
+
+  if (is_revcomp())
+    r.set_strand('-');
+  else
+    r.set_strand('+');
+
+  // if a read is mapped to - strand, bismark stores the + strand seq of
+  // reference genome rather than the seq of that read, however bsmap
+  // stores the read sequence. I'm not sure about the orientation of
+  // CIGAR string in bismark. But we do need the original sequence in .mr
+  if (is_revcomp() && mapper.compare("bismark") == 0)
+  {
+    revcomp_inplace(new_seq);
+    std::reverse(new_qual.begin(), new_qual.end());
+  }
+  r.set_score(mismatch);
+  mr.r = r;
+  mr.seq = new_seq;
+  mr.scr = new_qual;
+
+  if (mapper.compare("bsmap") == 0) {
+    string strand, bs_forward;
+    get_strand(strand, bs_forward);
+    if (is_pairend()) {
+      if (is_Trich()) {
+        mr.r.set_name(name + "/1");
+      }
+      else {
+        mr.r.set_name(name + "/2");
+      }
+      assert((is_Trich() && bs_forward == "+")
+        || (is_Arich() && bs_forward == "-"));
+    }
+    else {
+      //single end
+      assert(bs_forward == "+");
+      assert(is_revcomp() == (strand != bs_forward));
+      mr.r.set_name(name);
+    }
+    mr.r.set_strand(strand[0]);
+  }
+  else {
+    mr.r.set_name(name);
+  }
+
+  return mr;
+}
+
+bool
+SAM::load_read_from_line(std::istream& the_stream) {
+  if (mapper.compare("bsmap") == 0) {
+    if (!(the_stream >> name >> flag >> chrom >> start
+          >> mapq_score >> CIGAR >> mate_name >> mate_start
+          >> seg_len >> seq >> qual >> mismatch_str >> strand_str))
+      return false;
+
+    get_mismatch_bsmap();
+    return true;
+  }
+  else if (mapper.compare("bismark") == 0) {
+    if (!(the_stream >> name >> flag >> chrom >> start
+          >> mapq_score >> CIGAR >> mate_name
+          >> mate_start >> seg_len >> seq >> qual
+          >> edit_distance_str >> mismatch_str >> meth_call_str
+          >> read_conv_str >> genome_conv_str))
+      return false;
+
+    get_mismatch_bismark();
+    return true;
+  }
+  
+  return false;
+}
+
+void
+SAM::apply_CIGAR(string &new_seq, string &new_qual) const {
+    assert(seq.size() == qual.size());
+    assert(new_seq.size() == 0 && new_qual.size() == 0);
+    size_t n;
+    char op;
+    size_t i = 0;
+
+    std::istringstream iss(CIGAR);
+    while (iss >> n >> op)
+    {
+        switch (op)
+        {
+        case 'M':
+            new_seq += seq.substr(i, n);
+            new_qual += qual.substr(i, n);
+            i += n;
+            break;
+        case 'I':
+            i += n;
+            break;
+        case 'D':
+            new_seq += string(n, 'N');
+            new_qual += string(n, 'B');
+            break;
+        case 'S':
+            i += n;
+            break;
+        case 'H':
+            ;
+            break;
+        case 'P':
+            ;
+            break;
+        case '=':
+            ;
+            break;
+        case 'X':
+            ;
+            break;
+        }
+    }
+    
+    assert(i == seq.length());
+    assert(new_seq.size() == new_qual.size());
+}
+
+void
+SAM::get_mismatch_bsmap() {
+  mismatch = atoi(mismatch_str.substr(5).c_str());
+}
+
+void
+SAM::get_mismatch_bismark() {
+  /*
+  the result of this function might not be accurate, because if a sequencing
+  error occurs on a cytosine, then it probably will be reported as a convertion
+  */
+  size_t edit_distance;
+  edit_distance = atoi(edit_distance_str.substr(5).c_str());
+
+  int convert_count = 0;
+  const char *temp = meth_call_str.substr(5).c_str();
+  while(*temp != '\0') {
+    if (*temp == 'x' || *temp == 'h' || *temp == 'z')
+      ++convert_count;
+    ++temp;
+  }
+
+  mismatch = edit_distance - convert_count;
+}
+
+void
+SAM::get_strand(string &strand, string &bs_forward) const {
+  strand = strand_str.substr(5, 1);
+  bs_forward = strand_str.substr(6, 1);
+  if (bs_forward == "-") strand = strand == "+" ? "-" : "+";
+}
+
+std::istream& 
+operator>>(std::istream& the_stream, SAM &r) {
+  //To-do: do not load unmapped reads?
+  if (!r.load_read_from_line(the_stream))
+      //throw SMITHLABException("Unable to load read from input file.");
+      the_stream.setstate(std::ios::badbit);
+    
+    char c;
+    while ((c = the_stream.get()) != '\n' && the_stream);
+    
+    if (c != '\n')
+      //throw SMITHLABException("Unable to load read from input file.");
+      the_stream.setstate(std::ios::badbit);
+    
+    // the_stream.peek();
+    if (the_stream.eof())
+      //throw SMITHLABException("Unable to load read from input file.");
+      the_stream.setstate(std::ios::badbit);
+
+  return the_stream;
+}
+
+std::ostream& 
+operator<<(std::ostream& the_stream, SAM &r) {
+  //output as .mr format
+  MappedRead mr = r.GetMappedRead();
+
+  return the_stream << mr;
+}
---
+++ SAM.hpp
@@ -0,0 +1,102 @@
+/*
+ *    Part of SMITHLAB_CPP software
+ *
+ *    Copyright (C) 2013 University of Southern California and
+ *                       Andrew D. Smith
+ *
+ *    Authors: Meng Zhou, Qiang Song, Andrew Smith
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef SAM_HPP
+#define SAM_HPP
+
+#include "smithlab_utils.hpp"
+#include "MappedRead.hpp"
+
+#include <string>
+#include <vector>
+#include <fstream>
+#include <tr1/unordered_map>
+#include <limits>
+
+// All structs are written w/o consideration of header lines.
+class SAM {
+public:
+  //constructors
+  SAM() {}
+  SAM(std::string mapper);
+  SAM(std::string mapper, std::string line);
+
+  //accessors
+  std::string get_name() const {return name;}
+
+  //flag status checkers
+  bool is_pairend() const {return flag & 0x1;}
+  bool is_singlend() const {return !(is_pairend());}
+  bool is_mapping_paired() const {return flag & 0x2;}
+  bool is_unmapped() const {return flag & 0x4;}
+  bool is_mapped() const {return !(is_unmapped());}
+  bool is_revcomp() const {return flag & 0x10;}
+  bool is_Trich() const {return flag & 0x40;}
+  bool is_Arich() const {return flag & 0x80;}
+  bool is_secondary() const {return flag & 0x100;}
+  bool is_primary() const {return !(is_secondary());}
+
+  MappedRead GetMappedRead() const;
+  bool load_read_from_line(std::istream& the_stream);
+
+private:
+  //standard SAM format fields
+  std::string mapper;
+  std::string name;
+  std::string chrom;
+  std::string CIGAR;
+  std::string mate_name;
+  std::string seq;
+  std::string qual;
+
+  //bismark specific fields
+  std::string edit_distance_str;
+  std::string mismatch_str;
+  std::string meth_call_str;
+  std::string read_conv_str;
+  std::string genome_conv_str;
+
+  //bsmap specific fields
+  std::string strand_str;
+
+  //some other (standard) attributes
+  size_t flag;
+  size_t start;
+  size_t mapq_score;
+  size_t mate_start;
+  size_t mismatch;
+  int seg_len;
+
+  //functions to get MappedRead
+  void apply_CIGAR(std::string &new_seq, std::string &new_qual) const;
+  void get_mismatch_bsmap();
+  void get_mismatch_bismark();
+  void get_strand(std::string &strand, std::string &bs_forward) const;
+};
+
+std::istream& 
+operator>>(std::istream& the_stream, SAM &r);
+
+std::ostream& 
+operator<<(std::ostream& the_stream, SAM &r);
+
+#endif
