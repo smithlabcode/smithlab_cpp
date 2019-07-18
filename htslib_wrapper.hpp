3d6fc4796e180ffecf99715767740414595f4339
Andrew D Smith <andrewds@usc.edu>
2019-07-18 15:15:05 -0700

    This now has the stuff from SAM2

--- htslib_wrapper.hpp
+++ htslib_wrapper.hpp
@@ -1,6 +1,10 @@
-/*    Copyright (C) 2018 Andrew D. Smith
+/*
+ *    Part of SMITHLAB_CPP software
  *
- *    Authors: Andrew D. Smith
+ *    Copyright (C) 2013 University of Southern California and
+ *                       Andrew D. Smith
+ *
+ *    Authors: Meng Zhou, Qiang Song, Andrew Smith
  *
  *    This program is free software: you can redistribute it and/or modify
  *    it under the terms of the GNU General Public License as published by
@@ -11,32 +15,70 @@
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef HTSLIB_WRAPPER
-#define HTSLIB_WRAPPER
+#ifndef HTSLIB_WRAPPER_HPP
+#define HTSLIB_WRAPPER_HPP
+
+#include "smithlab_utils.hpp"
+#include "MappedRead.hpp"
 
 #include <string>
+#include <vector>
+#include <fstream>
 
 #include <htslib/sam.h>
+#include <htslib/hts.h>
 
-class GenomicRegion;
-class MappedRead;
+struct SAMRecord {
+  MappedRead mr;
+  bool is_Trich;
+  bool is_mapping_paired;
+  bool is_primary;
+  bool is_mapped;
+  int seg_len;
 
-struct HTSFile {
-  HTSFile(const std::string &fn);
-  ~HTSFile();
-  operator bool() const {return is_good;}
-
-  samFile *the_file;
-  bam_hdr_t *the_header;
-  bool is_good;
+  std::string get_name() const {return mr.r.get_name();}
+  std::string get_chrom() const {return mr.r.get_chrom();}
 };
 
-HTSFile &
-operator>>(HTSFile &h, GenomicRegion &r);
+class SAMReader {
+public:
+  SAMReader(const std::string &filename, const std::string &mapper);
+  ~SAMReader();
 
-HTSFile &
-operator>>(HTSFile &h, MappedRead &mr);
+  operator bool() const {return good;}
+
+  friend SAMReader &
+  operator>>(SAMReader& sam_stream, SAMRecord &samr);
+
+private:
+  // internal methods
+  bool
+  get_SAMRecord(const std::string&, SAMRecord&);
+  bool
+  get_SAMRecord_bsmap(const std::string&, SAMRecord&);
+  bool
+  get_SAMRecord_bismark(const std::string&, SAMRecord&);
+  bool
+  get_SAMRecord_bsseeker(const std::string&, SAMRecord&);
+  bool
+  get_SAMRecord_general(const std::string&, SAMRecord&);
+
+  // data
+  std::string filename;
+  std::string mapper;
+  bool good;
+
+  htsFile* hts;
+  bam_hdr_t *hdr;
+  bam1_t *b;
+};
+
+SAMReader &
+operator>>(SAMReader& sam_stream, SAMRecord &samr);
 
 #endif
