5b9b71f9ae74df286bffb42c2d3781199856c1a8
Andrew D Smith <andrewds@usc.edu>
2024-06-23 10:10:32 -0700

    Putting guards around parts of source that should not be formatted

--- smithlab_utils.cpp
+++ smithlab_utils.cpp
@@ -82,14 +82,16 @@ smithlab::get_fdr_cutoff(const size_t n_tests, vector<double> &pvals,
 char
 complement(int i) {
   static const int b2c_size = 20;
+  // clang-format off
   static const char b2c[] = {
     //A,  b,  C,  d,  e,  f,  g,  h,  i,  j,  k,  l,  m,  n,  o,  p,  q,  r,  s,  T
-    'T','N','G','N','N','N','C','N','N','N','N','N','N','N','N','N','N','N','N','A'
+     'T','N','G','N','N','N','C','N','N','N','N','N','N','N','N','N','N','N','N','A'
   };
   static const char b2cl[] = {
     //A,  b,  C,  d,  e,  f,  g,  h,  i,  j,  k,  l,  m,  n,  o,  p,  q,  r,  s,  T
-    't','n','g','n','n','n','c','n','n','n','n','n','n','n','n','n','n','n','n','a'
+     't','n','g','n','n','n','c','n','n','n','n','n','n','n','n','n','n','n','n','a'
   };
+  // clang-format on
   if (i - 'A' >= 0 && i - 'A' < b2c_size)
     return b2c[i - 'A'];
   else if (i - 'a' >= 0 && i - 'a' < b2c_size)
@@ -236,6 +238,7 @@ ProgressBar::report(std::ostream &out, const size_t i) {
     out << '\n';
 }
 
+// clang-format off
 char to_valid_five_letter[] = {
 /*  0*/ 'N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N', /* 15*/
 /* 16*/ 'N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N', /* 31*/
@@ -246,3 +249,4 @@ char to_valid_five_letter[] = {
 /* 96*/ 'N','A','N','C','N','N','N','G','N','N','N','N','N','N','N','N', /*111*/
 /*112*/ 'N','N','N','N','T','N','N','N','N','N','N','N','N','N','N','N'  /*127*/
 };
+// clang-format on
